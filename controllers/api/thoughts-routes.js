7bf16d2fc6626f38233280ce680643b21fdb25ee
Emma Olson <emma.olson794@gmail.com>
2022-07-02 15:24:15 -0500

    thoughts pushing to user thoughts array

--- controllers/api/thoughts-routes.js
+++ controllers/api/thoughts-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Thought } = require('../../models');
+const { Thought, User } = require('../../models');
 
 // GET    /api/thoughts
 // get all thoughts
@@ -13,11 +13,42 @@ router.get('/', (req, res) => {
         });
 });
 
+// GET   /api/thoughts/:id
+// get one thought by id
+router.get('/:id', ({ params }, res) => {
+    Thought.findOne({ _id: params.id })
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
 // POST    /api/thoughts
 // create a thought
 router.post('/', ({ body }, res) => {
     Thought.create(body)
-        .then(dbThoughtData => res.json(dbThoughtData))
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: body.userId },
+                { $push: { thoughts: _id }},
+                { new: true }
+            )
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        }) 
         .catch(err => {
             console.log(err);
             res.status(400).json(err);
